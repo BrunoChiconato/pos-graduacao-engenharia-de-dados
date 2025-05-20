2f0898ce5c1bda5d21a2c072d749c693b887d42b
Bruno Chiconato <brunochiconato01@gmail.com>
2025-05-20 18:49:18 -0300

    refactor(module-1): adjust challenge-2 code

--- python-for-data-engineering/module-1/challenge-2.py
+++ python-for-data-engineering/module-1/challenge-2.py
@@ -5,19 +5,24 @@ The program classifies the student's performance based on the average score:
     - 3 <= Average < 7: 'EXAM'
     - 7 <= Average <= 10: 'PASSED'
 """
-first_score = float(input("Enter your first score (0-10): "))
-second_score = float(input("Enter your second score (0-10): "))
-third_score = float(input("Enter your third score (0-10): "))
+def main():
+    first_score = float(input("Enter your first score (0-10): "))
+    second_score = float(input("Enter your second score (0-10): "))
+    third_score = float(input("Enter your third score (0-10): "))
 
-if first_score > 0 and second_score > 0 and third_score > 0:
-        avg_score = (first_score + second_score + third_score)/3
+    if first_score > 0 and second_score > 0 and third_score > 0:
+            avg_score = (first_score + second_score + third_score)/3
 
-        if avg_score < 3:
-            print("FAILED")
-        elif avg_score < 7:
-            print("EXAM")
-        else:
-            print("PASSED")
+            if avg_score < 3:
+                print("FAILED")
+            elif avg_score < 7:
+                print("EXAM")
+            else:
+                print("PASSED")
 
-else:
-    print("Negative inputs are not allowed!")
+    else:
+        print("Negative inputs are not allowed!")
+
+
+if __name__ == "__main__":
+    main()
